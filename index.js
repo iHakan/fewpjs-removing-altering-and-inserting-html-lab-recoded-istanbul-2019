fb430585448809680f0a1ed6ca3c14566e96001f
Hakan Yildiz <hakan@re-coded.com>
2020-01-29 12:31:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
 document.querySelector('main#main').remove();
 
-let element= document.createElement('body>h1#victory');
-document.body.appendChild(element);
+let newHeader= document.createElement('body>h1#victory');
+document.body.appendChild(newHeader);
