c4db738f47fdf19f72669846041af97efb3a08c5
Hakan Yildiz <hakan@re-coded.com>
2020-01-29 13:06:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ document.querySelector('main#main').remove();
 let newHeader = document.createElement('h1');
 newHeader.setAttribute('id', 'victory');
 document.body.appendChild('newHeader');
+newHeader.innerHTML= 'Hakan is the champion';
